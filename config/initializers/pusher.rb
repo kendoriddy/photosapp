d461548a81030847a006ebcccbee3c6b9216f020
Kehinde Onifade <onifkay@gmail.com>
2023-02-14 13:38:45 +0100

    update

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,9 +1,8 @@
 require 'pusher'
 
-pusher = Pusher::Client.new(
-  app_id: '1554351',
-  key: '9790d666332ccf97e4b5',
-  secret: '1ad26d23cb4cacacc81d',
-  cluster: 'eu',
-  encrypted: true
-)
+Pusher.app_id = '1554351'
+Pusher.key = '9790d666332ccf97e4b5'
+Pusher.secret = '1ad26d23cb4cacacc81d'
+Pusher.cluster = 'eu'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
